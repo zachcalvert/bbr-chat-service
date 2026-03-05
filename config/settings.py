54bfa4487337f3ed72f1f73a0169c3c6fc639859
Zach Calvert <zach.calvert@solvhealth.com>
2026-03-05 13:01:01 -0800

    update origins

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,11 @@ DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    'CSRF_TRUSTED_ORIGINS',
+    'https://bbr-chat-service.fly.dev',
+).split(',')
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
